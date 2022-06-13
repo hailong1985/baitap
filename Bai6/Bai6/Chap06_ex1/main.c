037e54cb18482f4636b34f7b6fc85de759aa6cd0
hailong1985 <hailong19852013@gmail.com>
2022-06-13 18:14:07 +0700

    bai 6

---
+++ Bai6/Bai6/Chap06_ex1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	int a = 10;
+	float b = 246789234;
+	char ch = 'A';
+	printf("\nInteger data = %d", a);
+	printf("\nFloat Data = %f", b);
+	printf("\nThis prints the string");
+	printf("%s", "\nThis also prints a string");
+}
